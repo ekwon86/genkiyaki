9bdc0412690fd7fb089002a4bacb263d43f05317
Eugene Kwon <ekwon86@gmail.com>
2017-09-21 14:45:44 -0700

    test

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
-// PathLocationStrategy - Angular uses this for routing
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
