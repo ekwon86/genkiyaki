aa0577a74850e3bdc02b8867fc36d018986eaa55
Gene Kwon <ekwon86@gmail.com>
2017-09-25 21:11:10 -0700

    fixed picture container

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -7,14 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  pictureArray = [];
+  pictureArray = [
+    {url: '../../../assets/bbqplate.jpg'},
+    {url: '../../../assets/bbqplate2.jpg'},
+    {url: '../../../assets/chxbowl.jpg'},
+    {url: '../../../assets/taco.jpg'},
+    {url: '../../../assets/fries.jpg'},
+    {url: '../../../assets/taco2.jpg'},
+    {url: '../../../assets/taco.jpg'},
+    {url: '../../../assets/chxplate.jpg'},
+    {url: '../../../assets/taco2.jpg'},
+    {url: '../../../assets/taco3.jpg'},
+    {url: '../../../assets/chxplate.jpg'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
   pictureClicked(event) {
-    
+    console.log(this.pictureArray);
   }
 
 }
